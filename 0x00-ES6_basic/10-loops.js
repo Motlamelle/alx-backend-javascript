f07127f09829ee1e34c0fa8ea36227adc614a816
Motlamelle <lebomotlamelle@gmail.com>
2023-01-18 21:17:54 +0200

    update

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,10 @@
-export default function appendToEachArrayValue(array, appendString) {
-    const returnArray = [];
-    for (const element of array) {
-      returnArray.push(appendString + element);
-    }
-  
-    return returnArray;
+
+ORDER BY lifespan DESC;
+rt default function appendToEachArrayValue(array, appendString) {
+  const returnArray = [];
+  for (const element of array) {
+    returnArray.push(appendString + element);
   }
+
+  return returnArray;
+}
